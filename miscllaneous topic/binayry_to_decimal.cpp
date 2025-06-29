3b467a2bae3c2ac912e4283eb1996c3ba970cb9b
Prakash <109280432+prakash182@users.noreply.github.com>
2025-06-29 18:35:20 +0000

    binary to deimal

---
+++ miscllaneous topic/binayry_to_decimal.cpp
@@ -0,0 +1,26 @@
+// binary to decimal conversion
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+
+  int ans = 0;
+  int pow = 1;
+
+ while(n > 0)
+  {
+     int rem = n % 10;
+
+     ans += (rem*pow);
+
+     n /= 10;
+     pow = pow*2;
+
+  }
+
+  cout<<ans<<endl;
+}
